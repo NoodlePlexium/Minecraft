90b64ad045ae81c559e0698e40e59263ed7041ac
Arran Stevens <55632520+NoodlePlexium@users.noreply.github.com>
2023-06-11 01:23:49 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,34 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-#include "window.h"
+#include "GameWindow.h"
+//#include "camera.h"
+//#include "Vector.h"
+//#include "renderer.h"
+#include "pipeline.h"
 
 int main() 
 {
 
-    Window window(800, 600, "Minecraft");
+    int width = 800;
+    int height = 600;
+
+    GameWindow window(width, height, "Minecraft");
     window.Run();
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    // Render Pipeline
+    RenderPipeline renderPipeline("shaders/shader.vert.spv", "shaders/shader.frag.spv");
 
-    std::cout << extensionCount << " extensions supported\n";
+    //uint32_t extensionCount = 0;
+    //vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+
+    // Set up camera
+
+    //Vector camPos(0, 0, 0);
+    //Vector canRot(0, 0, 0);
+    //Camera camera(camPos, camRot, width, height);
+    return 0;
 }
 
 // cd ..
