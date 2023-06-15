4a760b9efeabe9e94fb0d0fabf607b0fa7387938
Arran Stevens <55632520+NoodlePlexium@users.noreply.github.com>
2023-06-15 13:16:12 +0200

    Update GameWindow.h

--- src/GameWindow.h
+++ src/GameWindow.h
@@ -29,20 +29,35 @@ public:
 
 	VkExtent2D getExtent() {return {static_cast<uint32_t>(width), static_cast<uint32_t>(height)};}
 
+	bool wasWindowResized() {return framebufferResized;}
+	void resetWindowResizedFlag() {framebufferResized = false;}
+
+	GLFWwindow *getGLFWwindow() const {return window;}
 
 private:
+
+	static void framebufferResizeCallBack(GLFWwindow *window, int width, int height){
+		auto gameWindow = reinterpret_cast<GameWindow *>(glfwGetWindowUserPointer(window));
+		gameWindow->framebufferResized = true;
+		gameWindow->width = width;
+		gameWindow->height = height;
+	}
 
 	void Init(){
 		glfwInit();
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
 		window = glfwCreateWindow(width, height, windowName.c_str(), nullptr, nullptr);
+		glfwSetWindowUserPointer(window, this);
+		glfwSetFramebufferSizeCallback(window, framebufferResizeCallBack);
 	}
 
 
-	const int width;
-	const int height;
+	int width;
+	int height;
+	bool framebufferResized = false;
+
 	std::string windowName;
 	GLFWwindow *window;
 };
