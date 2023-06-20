2775ad09943aa8aed8348b1476ff729d0b0fc834
Arran Stevens <55632520+NoodlePlexium@users.noreply.github.com>
2023-06-21 00:43:04 +0200

    Update render_system.h

--- src/render_system.h
+++ src/render_system.h
@@ -10,6 +10,9 @@
 #include "engine_device.h"
 #include "engine_game_object.h"
 #include "camera.h"
+#include "engine_frame_info.h"
+#include "terrain.h"
+
 
 #include <memory>
 #include <vector>
@@ -20,16 +23,16 @@
 namespace Engine{
 
 struct SimplePushConstantData{
-	glm::mat4 transform{1.f};
-	alignas(16) glm::vec3 colour;
+	glm::mat4 meshMatrix{1.0f};
+	glm::mat4 normalMatrix{1.0f};
 };
 
 class RenderSystem{
 public:
 
-	RenderSystem(EngineDevice& device, VkRenderPass renderPass) : engineDevice{device} 
+	RenderSystem(EngineDevice& device, VkRenderPass renderPass, VkDescriptorSetLayout globalSetLayout) : engineDevice{device} 
 	{
-		createPipelineLayout();
+		createPipelineLayout(globalSetLayout);
 		createPipeline(renderPass);
 	}
 
@@ -40,48 +43,75 @@ public:
 	RenderSystem &operator=(const RenderSystem &) = delete;	
 
 
-	void renderGameObjects(
-		VkCommandBuffer commandBuffer, 
-		std::vector<EngineGameObject>& gameObjects, 
-		const Camera &camera)
+	void renderGameObjects(FrameInfo &frameInfo, std::vector<EngineGameObject>& gameObjects, Terrain& terrain)
 	{
-		graphicsPipeline->bind(commandBuffer);
+		graphicsPipeline->bind(frameInfo.commandBuffer);
 
-		auto projectionView = camera.getProjection() * camera.getView();
+		vkCmdBindDescriptorSets(
+			frameInfo.commandBuffer,
+			VK_PIPELINE_BIND_POINT_GRAPHICS,
+			pipelineLayout,
+			0, 
+			1, 
+			&frameInfo.globalDescriptorSet,
+			0, 
+			nullptr);
 
+		// Render game objects
 		for (auto& obj : gameObjects){
 
 			SimplePushConstantData push{};
-			push.colour = obj.colour;
-			push.transform = projectionView * obj.transform.mat4();
+			push.meshMatrix = obj.transform.mat4();
+			push.normalMatrix = obj.transform.normalMatrix();
 
 			vkCmdPushConstants(
-				commandBuffer, 
+				frameInfo.commandBuffer, 
 				pipelineLayout, 
 				VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 
 				0, 
 				sizeof(SimplePushConstantData), 
 				&push);
-			obj.model->bind(commandBuffer);
-			obj.model->draw(commandBuffer);
+			obj.mesh->bind(frameInfo.commandBuffer);
+			obj.mesh->draw(frameInfo.commandBuffer);
+		}
+
+		// Render terrain
+		for (auto& obj : terrain.chunkObjects){
+
+			SimplePushConstantData push{};
+			push.meshMatrix = obj.transform.mat4();
+			push.normalMatrix = obj.transform.normalMatrix();
+
+			vkCmdPushConstants(
+				frameInfo.commandBuffer, 
+				pipelineLayout, 
+				VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 
+				0, 
+				sizeof(SimplePushConstantData), 
+				&push);
+			obj.mesh->bind(frameInfo.commandBuffer);
+			obj.mesh->draw(frameInfo.commandBuffer);
 		}
 	}
 
 
 private:
 
-	void createPipelineLayout(){
+	void createPipelineLayout(VkDescriptorSetLayout globalSetLayout) {
 
 		VkPushConstantRange pushConstantRange{};
 		pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
 		pushConstantRange.offset = 0;
 		pushConstantRange.size = sizeof(SimplePushConstantData);
 
+		std::vector<VkDescriptorSetLayout> descriptorSetLayouts{globalSetLayout};
+
+
 
 		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
 		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-		pipelineLayoutInfo.setLayoutCount = 0;
-		pipelineLayoutInfo.pSetLayouts = nullptr;
+		pipelineLayoutInfo.setLayoutCount = static_cast<uint32_t>(descriptorSetLayouts.size());
+		pipelineLayoutInfo.pSetLayouts = descriptorSetLayouts.data();
 		pipelineLayoutInfo.pushConstantRangeCount = 1;
 		pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
 		if (vkCreatePipelineLayout(engineDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS){
@@ -89,7 +119,7 @@ private:
 		}
 	}
 
-	void createPipeline(VkRenderPass renderPass){
+	void createPipeline(VkRenderPass renderPass) {
 		assert(pipelineLayout != nullptr && "Cannot create pipeline before pipeline layout!");
 
 		PipelineConfigInfo pipelineConfig{};
