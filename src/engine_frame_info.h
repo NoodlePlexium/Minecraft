16472f86f5696be87711c697f35304db97a3d528
Arran Stevens <55632520+NoodlePlexium@users.noreply.github.com>
2023-06-21 00:45:07 +0200

    Add files via upload

---
+++ src/engine_frame_info.h
@@ -0,0 +1,20 @@
+#ifndef ENGINE_FRAME_INFO_H
+#define ENGINE_FRAME_INFO_H
+
+#include "camera.h"
+#include <vulkan/vulkan.h>
+
+namespace Engine{
+
+
+struct FrameInfo {
+	int frameIndex;
+	float frameTime;
+	VkCommandBuffer commandBuffer;
+	Camera &camera;
+	VkDescriptorSet globalDescriptorSet;
+};
+
+
+} // namespace	
+#endif
